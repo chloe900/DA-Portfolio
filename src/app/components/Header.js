5bd9c3dba4f8093648d93c00785109dd218d8f8c
chloe900 <chloetdube@icloud.com>
2025-01-11 22:01:27 +0200

    first draft of the header

---
+++ src/app/components/Header.js
@@ -0,0 +1,20 @@
+// filepath: /c:/Users/Taboka Chloé Dube/Desktop/DA-Portfolio/src/index.js
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+export default function Header() {
+  return (
+    <header className="bg-[#a5b4fc] text-white p-12 border-b-2 border-[#020617]">
+      <nav className="max-w-6xl mx-auto flex justify-between">
+        <div className="flex items-center space-x-4">
+          <i className="fa-brands fa-codepen h-8 w-8 mt-4 text-xl"></i>
+          <h1 className="text-xl font-serif">Chloe's Portfolio</h1>
+        </div>
+        <span class = "mt-4 font-serif text-xl">
+        <button class="bg-[#020617] hover:bg-red-700 text-white font-bold py-3 px-12 rounded-full">Menu</button>
+        </span>
+      </nav>
+    </header>
+  );
+}
+  
