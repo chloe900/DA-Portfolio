d0f01a871064af1acb753872cf61d9ab368dceed
chloe900 <chloetdube@icloud.com>
2025-01-12 10:11:46 +0200

    updated text color

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer(){
     return(
-        <footer className = "bg-[#F4F5FF] bg-cover h-50 flex text-white border-t-2 border-gray-800 text-[#1e3a8a] flex flex-col justify-center items-center">
+        <footer className = "bg-[#F4F5FF] bg-cover h-50 flex border-t-2 border-gray-800 text-[#1e3a8a] flex flex-col justify-center items-center">
             <h1 className = "relative text-l font-serif ml-50">Contact</h1>
             <h1 className = "mb-10 relative text-l font-serif px-10"> <a href="mailto:chloetdube@gmail.com" className="text-xl font-serif px-10 text-[#1e3a8a]">chloetdube@gmail.com</a></h1>
             <p className = "text-gray-800 relative text-xs font-serif px-10">Website Made by: Chloe Dube</p>
